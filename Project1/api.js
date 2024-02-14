c25b74b2763ec339da52af331f1dbddfe5d8b516
John Wick <125253453+Jhon-wick007@users.noreply.github.com>
2024-02-14 15:00:16 +0530

    Add files via upload
    This project is based upon attempting to re-create a movie website with HTML,CSS and JS and calling the Front-end API through a movies database using as a public key

---
+++ Project1/api.js
@@ -0,0 +1,61 @@
+const API_LINK='https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=41ee980e4b5f05f6693fda00eb7c4fd4&page=1';
+const IMG_PATH="https://image.tmdb.org/t/p/w1280";
+const SEARCH_API="https://api.themoviedb.org/3/search/movie?&api_key=41ee980e4b5f05f6693fda00eb7c4fd4&query=";
+
+const main=document.getElementById("section");
+const form=document.getElementById("frm");
+const search=document.getElementById("s1");
+returnmovies(API_LINK);
+
+
+function crete(data)
+{
+    console.log(data.results);
+    let obj=data.results;
+    obj.forEach(element=>{
+        const div_card=document.createElement('div');
+            div_card.setAttribute('class','card');
+
+            const div_row=document.createElement('div');
+            div_row.setAttribute('class','row');
+
+            const div_column=document.createElement('div');
+            div_column.setAttribute('class','column');
+
+            const image=document.createElement('img');
+            image.setAttribute('class','thumbnail');
+            image.setAttribute('id','img');
+
+            const title=document.createElement('h3');
+            title.setAttribute('id','title');
+            
+            const center=document.createElement('center');
+
+            title.innerHTML=`${element.title}`
+            image.src=IMG_PATH+element.poster_path;
+            center.appendChild(image);
+            div_card.appendChild(center);
+            div_card.appendChild(title);
+            div_column.appendChild(div_card);
+            div_row.appendChild(div_column);
+            main.appendChild(div_row);
+    });
+}
+async function returnmovies(url)
+{
+let x= await fetch(url);
+let y= await x.json();
+
+crete(y);
+}
+
+form.addEventListener("submit",function(event){
+    event.preventDefault();
+    main.innerHTML='';
+
+    const searchitem=search.value;
+    if(searchitem){
+        returnmovies(SEARCH_API+searchitem);
+        search.value="";
+    }
+});
